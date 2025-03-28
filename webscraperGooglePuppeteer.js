a0cdb8431f88a705493cc31bdf350bd9948cf30b
Felipe <feliperosenek@gmail.com>
2025-03-27 23:07:23 -0300

    Deploy

--- webscraperGooglePuppeteer.js
+++ webscraperGooglePuppeteer.js
@@ -26,7 +26,7 @@ const pool2 = new Pool({
     port: 5432,
     connectionTimeoutMillis: 180000
 });
-
+console.log("v1")
 /**
  * Função para extrair informações de telefone de um texto
  * @param {string} texto - O texto contendo números de telefone
