e1c1368732b7177302e509b9e8db37b4398dde67
Felipe <feliperosenek@gmail.com>
2025-03-29 18:42:46 -0300

    Deploy

--- webscraperGoogleCheerio.js
+++ webscraperGoogleCheerio.js
@@ -25,7 +25,7 @@ const pool2 = new Pool({
     port: 5432,
     connectionTimeoutMillis: 180000
 });
-console.log("v1.0.3")
+console.log("v1.0.5")
 /**
  * Função para extrair informações de telefone de um texto
  * @param {string} texto - O texto contendo números de telefone
